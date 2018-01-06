b2c6b20d7eaf437da698506fe64742855da90030
pkvarnwal <pkvarnwal@gmail.com>
2018-01-06 15:14:27 +0530

    Feature : ViewPager : Added ViewPager screen

---
+++ app/src/main/java/com/linkmindpro/activities/ViewPagerActivity.java
@@ -0,0 +1,23 @@
+package com.linkmindpro.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import constraint.com.linkmindpro.R;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+
+        initView();
+    }
+
+    private void initView() {
+
+    }
+
+}
