d219453f59e872ab2a470546a375c80108ce5324
pkvarnwal <pkvarnwal@gmail.com>
2018-01-27 01:12:40 +0530

    Feature : EditProfile : Implemented Api for EditProfile

---
+++ app/src/main/java/com/linkmindpro/models/editprofile/EditProfileData.java
@@ -0,0 +1,97 @@
+package com.linkmindpro.models.editprofile;
+
+import com.google.gson.annotations.SerializedName;
+
+public class EditProfileData {
+
+    @SerializedName("id") private String id;
+    @SerializedName("name") private String name;
+    @SerializedName("email") private String email;
+    @SerializedName("profession") private String profession;
+    @SerializedName("image") private String image;
+    @SerializedName("address") private String address;
+    @SerializedName("zipcode") private String zipcode;
+    @SerializedName("city") private String city;
+    @SerializedName("state") private String state;
+    @SerializedName("phone") private String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
